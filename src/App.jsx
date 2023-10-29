35ac6c8f7e6207ed708e5fd48c97dd52a49013cb
WilliansAS <anthowill1608@gmail.com>
2023-10-29 01:50:40 -0500

    se agrego el Index

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Nosotros from './pages/Nosotros';
 import Contacto from './pages/Contacto';
 import Error from './pages/Error';
 import Servicios from './pages/Servicios'
+import Index from './pages/Index';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path='/' element={<Index/>} />
       <Route path='/registro' element={<Registro/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/nosotros' element={<Nosotros/>} />
