2817322db81d236309a2114f09a0451f39cdeccf
WilliansAS <anthowill1608@gmail.com>
2023-11-06 11:02:07 -0500

    Se creo el Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,197 +1,132 @@
-import React from "react";
-import "../styles/argon-dashboard.css?v=1.0.1"
+import React, { useState } from 'react';
+import MenuDash from "../components/MenuDash";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faUsers, faBookMedical, faKitMedical, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import "../styles/dashboard.css";
 
 function Dashboard (){
+const [profile, setProfile] = useState(JSON.parse(localStorage.getItem('userProfile')) || {});
+
+  const logOut = () => {
+    googleLogout();
+    localStorage.removeItem('userProfile');
+    setProfile([]);
+  };
+
     return(
         <>
-<body class="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500">
+       <MenuDash />
+       <main className="menu-conte"> 
+       <div className="header-wrapper">     
+          <div className="header-titulo">
+            <h1>Dashboard, Bienvenido {profile.name}</h1>
+        </div>
+        <div className="busca">
+          <i><FontAwesomeIcon icon={faMagnifyingGlass} /></i>
+          <input type="text" placeholder="Buscar" />
+        </div>
+        </div> 
 
-  <div className="absolute w-full bg-blue-500 dark:hidden min-h-75" />
-  {/* sidenav  */}
-  <aside
-    className="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0"
-    aria-expanded="false"
-  >
-    <div className="h-19">
-      <i
-        className="absolute top-0 right-0 p-4 opacity-50 cursor-pointer fas fa-times dark:text-white text-slate-400 xl:hidden"
-        sidenav-close=""
-      />
-      <a
-        className="block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700"
-        href="https://demos.creative-tim.com/argon-dashboard-tailwind/pages/dashboard.html"
-        target="_blank"
-      >
-        <img
-          src="../img/logo-ct-dark.png"
-          className="inline h-full max-w-full transition-all duration-200 dark:hidden ease-nav-brand max-h-8"
-          alt="main_logo"
-        />
-        <img
-          src="../img/logo-ct.png"
-          className="hidden h-full max-w-full transition-all duration-200 dark:inline ease-nav-brand max-h-8"
-          alt="main_logo"
-        />
-        <span className="ml-1 font-semibold transition-all duration-200 ease-nav-brand">
-          Dashboard
-        </span>
-      </a>
-    </div>
-    <hr className="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent" />
-    <div className="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
-      <ul className="flex flex-col pl-0 mb-0">
-        <li className="mt-0.5 w-full">
-          <a
-            className="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
-            href="../pages/dashboard.html"
-          >
-            <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-              <i className="relative top-0 text-sm leading-normal text-blue-500 ni ni-tv-2" />
+        
+      {/* Cartas contenedir */}
+
+      <div className="carta-cont">
+        <h1 className="main-title">Datos</h1>
+        <div className="carta-wrapper">
+
+          <div className="carta-payment light-green">
+            <div className="carta-header">
+              <div className="amount">
+                <span className="ctitulo">Usuarios</span>
+                <span className="amount-valor">1356</span>
+              </div>
+              <i className="cartaicon"><FontAwesomeIcon icon={faUsers} /></i>
             </div>
-            <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-              Dashboard
-            </span>
-          </a>
-        </li>
-        <li className="mt-0.5 w-full">
-          <a
-            className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-            href="../pages/tables.html"
-          >
-            <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-              <i className="relative top-0 text-sm leading-normal text-orange-500 ni ni-calendar-grid-58" />
-            </div>
-            <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-              Tables
-            </span>
-          </a>
-        </li>
-        <li className="mt-0.5 w-full">
-          <a
-            className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-            href="../pages/billing.html"
-          >
-            <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center fill-current stroke-0 text-center xl:p-2.5">
-              <i className="relative top-0 text-sm leading-normal text-emerald-500 ni ni-credit-card" />
-            </div>
-            <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-              Billing
-            </span>
-          </a>
-        </li>
-        <li className="mt-0.5 w-full">
-          <a
-            className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-            href="../pages/virtual-reality.html"
-          >
-            <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-              <i className="relative top-0 text-sm leading-normal text-cyan-500 ni ni-app" />
-            </div>
-            <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-              Virtual Reality
-            </span>
-          </a>
-        </li>
-        <li className="mt-0.5 w-full">
-          <a
-            className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-            href="../pages/rtl.html"
-          >
-            <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-              <i className="relative top-0 text-sm leading-normal text-red-600 ni ni-world-2" />
-            </div>
-            <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-              RTL
-            </span>
-          </a>
-        </li>
-        <li className="w-full mt-4">
-          <h6 className="pl-6 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">
-            Account pages
-          </h6>
-        </li>
-        <li className="mt-0.5 w-full">
-          <a
-            className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-            href="../pages/profile.html"
-          >
-            <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-              <i className="relative top-0 text-sm leading-normal text-slate-700 ni ni-single-02" />
-            </div>
-            <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-              Profile
-            </span>
-          </a>
-        </li>
-        <li className="mt-0.5 w-full">
-          <a
-            className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-            href="../pages/sign-in.html"
-          >
-            <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-              <i className="relative top-0 text-sm leading-normal text-orange-500 ni ni-single-copy-04" />
-            </div>
-            <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-              Sign In
-            </span>
-          </a>
-        </li>
-        <li className="mt-0.5 w-full">
-          <a
-            className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-            href="../pages/sign-up.html"
-          >
-            <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-              <i className="relative top-0 text-sm leading-normal text-cyan-500 ni ni-collection" />
-            </div>
-            <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-              Sign Up
-            </span>
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div className="mx-4">
-      {/* load phantom colors for card after: */}
-      <p className="invisible hidden text-gray-800 text-red-500 text-red-600 text-blue-500 dark:bg-white bg-slate-500 bg-gray-500/30 bg-cyan-500/30 bg-emerald-500/30 bg-orange-500/30 bg-red-500/30 after:bg-gradient-to-tl after:from-zinc-800 after:to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 after:from-blue-700 after:to-cyan-500 after:from-orange-500 after:to-yellow-500 after:from-green-600 after:to-lime-400 after:from-red-600 after:to-orange-600 after:from-slate-600 after:to-slate-300 text-emerald-500 text-cyan-500 text-slate-400" />
-      <div
-        className="relative flex flex-col min-w-0 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border"
-        sidenav-card=""
-      >
-        <img
-          className="w-1/2 mx-auto"
-          src="../assets/img/illustrations/icon-documentation.svg"
-          alt="sidebar illustrations"
-        />
-        <div className="flex-auto w-full p-4 pt-0 text-center">
-          <div className="transition-all duration-200 ease-nav-brand">
-            <h6 className="mb-0 dark:text-white text-slate-700">Need help?</h6>
-            <p className="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">
-              Please check our docs
-            </p>
+            <span className="carta-detalle">+55% de usuarios</span>
           </div>
+
+          <div className="carta-payment light-blue">
+            <div className="carta-header">
+              <div className="amount">
+                <span className="ctitulo">Servicios</span>
+                <span className="amount-valor">25</span>
+              </div>
+              <i className="cartaicon dark-blue"><FontAwesomeIcon icon={faKitMedical} /></i>
+            </div>
+            <span className="carta-detalle">**** **** **** 2321</span>
+          </div>
+
+          <div className="carta-payment light-red">
+            <div className="carta-header">
+              <div className="amount">
+                <span className="ctitulo">Artículos</span>
+                <span className="amount-valor">2500</span>
+              </div>
+              <i className="cartaicon dark-red"><FontAwesomeIcon icon={faBookMedical} /></i>
+            </div>
+            <span className="carta-detalle">**** **** **** 2321</span>
+          </div>
+
+          <div className="carta-payment light-purple">
+            <div className="carta-header">
+              <div className="amount">
+                <span className="ctitulo">Nuevos usuarios</span>
+                <span className="amount-valor">35</span>
+              </div>
+              <i className="cartaicon dark-purple"><FontAwesomeIcon icon={faUserPlus} /></i>
+            </div>
+            <span className="carta-detalle">+12.4% de nuevos usuarios</span>
+          </div>
+        </div>  
+      </div>
+
+      {/* Tabla */}
+      <div className="tabla-wrapper">
+        <h3 className="tabla-titulo">APIs Del Proyecto</h3>
+        <div className="tabla-cont">
+          <table>
+            <thead>
+              <tr>
+                <th>API</th>
+                <th>Descripción</th>
+                <th>Estatus</th>
+                <th>Categoría</th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr>
+                  <td>Google</td>
+                  <td>Esta API ofrece una forma segura y confiable de autenticación, permitiendo a los usuarios iniciar sesión en la plataforma con sus cuentas de Google existentes.</td>
+                  <td>Disponible</td>
+                  <td>Login</td>
+                </tr>
+
+                <tr>
+                  <td>Facebook</td>
+                  <td>Esta API permite a los usuarios iniciar sesión en sus cuentas de Med-Health utilizando sus credenciales de Facebook.</td>
+                  <td>Disponible</td>
+                  <td>Login</td>
+                </tr>
+
+                <tr>
+                  <td>EmailJS</td>
+                  <td>Esta API permite a nuestro sistema de salud y medicina enviar notificaciones por correo electrónico de manera eficiente, lo que garantiza una comunicación efectiva con nuestros usuarios y profesionales de la salud.</td>
+                  <td>Disponible</td>
+                  <td>Comunicación</td>
+                </tr>
+
+                <tr>
+                  <td>Google Books</td>
+                  <td>Esta API permite a los usuarios buscar, explorar y obtener información detallada sobre libros relacionados con la salud y la medicina.</td>
+                  <td>Disponible</td>
+                  <td>Información</td>
+                </tr>
+              </tbody>
+          </table>
         </div>
       </div>
-      <a
-        href="https://www.creative-tim.com/learning-lab/tailwind/html/quick-start/argon-dashboard/"
-        target="_blank"
-        className="inline-block w-full px-8 py-2 mb-4 text-xs font-bold leading-normal text-center text-white capitalize transition-all ease-in rounded-lg shadow-md bg-slate-700 bg-150 hover:shadow-xs hover:-translate-y-px"
-      >
-        Documentation
-      </a>
-      {/* pro btn  */}
-      <a
-        className="inline-block w-full px-8 py-2 text-xs font-bold leading-normal text-center text-white align-middle transition-all ease-in bg-blue-500 border-0 rounded-lg shadow-md select-none bg-150 bg-x-25 hover:shadow-xs hover:-translate-y-px"
-        href="https://www.creative-tim.com/product/argon-dashboard-pro-tailwind?ref=sidebarfree"
-        target="_blank"
-      >
-        Upgrade to pro
-      </a>
-    </div>
-  </aside>
-
-  </body>
-</>
+    </main>
+       </>
       
     )
 }
