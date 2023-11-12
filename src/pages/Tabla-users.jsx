1e2fc5a82d6615c8b5a3e118073f28181495624e
WilliansAS <anthowill1608@gmail.com>
2023-11-12 15:07:57 -0500

    Borrar y leer tabla usuarios

--- src/pages/Tabla-users.jsx
+++ src/pages/Tabla-users.jsx
@@ -1,50 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MenuDash from "../components/MenuDash";
+import axios from "axios";
+import imagen from "../img/header.png";
 
 function TablaUsers(){
 
-    const tableItems = [
-        {
-            avatar: "https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
-            name: "Liam James",
-            email: "liamjames@example.com",
-            phone_nimber: "+1 (555) 000-000",
-            position: "Software engineer",
-            salary: "$100K"
-        },
-        {
-            avatar: "https://randomuser.me/api/portraits/men/86.jpg",
-            name: "Olivia Emma",
-            email: "oliviaemma@example.com",
-            phone_nimber: "+1 (555) 000-000",
-            position: "Product designer",
-            salary: "$90K"
-        },
-        {
-            avatar: "https://randomuser.me/api/portraits/women/79.jpg",
-            name: "William Benjamin",
-            email: "william.benjamin@example.com",
-            phone_nimber: "+1 (555) 000-000",
-            position: "Front-end developer",
-            salary: "$80K"
-        },
-        {
-            avatar: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
-            name: "Henry Theodore",
-            email: "henrytheodore@example.com",
-            phone_nimber: "+1 (555) 000-000",
-            position: "Laravel engineer",
-            salary: "$120K"
-        },
-        {
-            avatar: "https://images.unsplash.com/photo-1439911767590-c724b615299d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
-            name: "Amelia Elijah",
-            email: "amelia.elijah@example.com",
-            phone_nimber: "+1 (555) 000-000",
-            position: "Open source manager",
-            salary: "$75K"
-        },
-    ]
+     //Obtener usuarios
+  const [ Users, setUsers ] = useState([]);
+  useEffect( () => {
+    fetchUsers();
+  },[]);
+
+  const fetchUsers = async () => {
+    
+    const response = await axios.get('http://localhost:3000/users')
+    setUsers(response.data)
+    console.log(response)    
+  }
+
+   //Eliminar usuarios
+   const HandeDelete = async (id) => {
+    const response = await axios.delete(`http://localhost:3000/users/${id}`);
+
+    if(response.status == 200){
+      alert("El usuario se borro correctamente");
+    }else{
+      alert("Error");
+    }
+    fetchUsers();
+  }
 
     return(
         <>
@@ -74,31 +58,30 @@ function TablaUsers(){
                         <tr>
                             <th className="py-3 px-6">Username</th>
                             <th className="py-3 px-6">Email</th>
-                            <th className="py-3 px-6">Position</th>
-                            <th className="py-3 px-6">Salary</th>
+                            <th className="py-3 px-6">Apellido</th>
+                            <th className="py-3 px-6">Password</th>
                             <th className="py-3 px-6"></th>
 
                         </tr>
                     </thead>
                     <tbody className="text-gray-600 divide-y">
-                        {
-                            tableItems.map((item, idx) => (
-                                <tr key={idx}>
+                            {Users.map((users) =>(
+                                <tr key={users.id}>
                                     <td className="flex items-center gap-x-3 py-3 px-6 whitespace-nowrap">
-                                        <img src={item.avatar} className="w-10 h-10 rounded-full" />
+                                        <img src={imagen} className="w-10 h-10 rounded-full" />
                                         <div>
-                                            <span className="block text-gray-700 text-sm font-medium">{item.name}</span>
-                                            <span className="block text-gray-700 text-xs">{item.email}</span>
+                                            <span className="block text-gray-700 text-sm font-medium">{users.UserName}</span>
+                                            <span className="block text-gray-700 text-xs">{users.email || "email@gmail.com"}</span>
                                         </div>
                                     </td>
-                                    <td className="px-6 py-4 whitespace-nowrap">{item.phone_nimber}</td>
-                                    <td className="px-6 py-4 whitespace-nowrap">{item.position}</td>
-                                    <td className="px-6 py-4 whitespace-nowrap">{item.salary}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{users.Nombre}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{users.Apellido}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{users.Password}</td>
                                     <td className="text-right px-6 whitespace-nowrap">
-                                        <a href="javascript:void()" className="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg">
+                                        <a className="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg">
                                             Editar
                                         </a>
-                                        <button href="javascript:void()" className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg">
+                                        <button onClick={() => HandeDelete(users.id)} className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg">
                                             Eliminar
                                         </button>
                                     </td>
