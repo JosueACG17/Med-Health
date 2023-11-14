b9b72f5ad1155f61186018d37ccd642e1a131ccd
Garcia Canul Guillermo de Jesus <22393204@utcancun.edu.mx>
2023-11-14 11:05:57 -0500

    Correcion App

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,15 @@ import Error from './pages/Error';
 import Servicios from './pages/Servicios'
 import Index from './pages/Index';
 import Libreria from './pages/Libreria';
-<<<<<<< HEAD
-import Companieros from './pages/Companieros';
-import Modal from './components/Modal';
-=======
 import Dashboard from './pages/Dashboard';
 import TablaUsers from './pages/Tabla-users';
->>>>>>> 03cdda33d1082e87944dc4d99f616b2696aae244
+import Companieros from './pages/Companieros';
+import Modal from './components/Modal';
 
 function App() {
 
   return (
     <BrowserRouter>
-<<<<<<< HEAD
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/home' element={<Index />} />
@@ -28,24 +24,12 @@ function App() {
         <Route path='/contacto' element={<Contacto />} />
         <Route path='/servicios' element={<Servicios />} />
         <Route path='/libreria' element={<Libreria />} /> 
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/usuarios' element={<TablaUsers/>} />
         <Route path='/companieros' element={<Companieros />} />
         <Route path='/modal' element={<Modal/>}/>
         <Route path='*' element={<Error />} />
       </Routes>
-=======
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/home' element={<Index />} />
-      <Route path='/registro' element={<Registro/>} />
-      <Route path='/nosotros' element={<Nosotros/>} />
-      <Route path='/contacto' element={<Contacto/>} />
-      <Route path='/servicios' element={<Servicios/>} />
-      <Route path='/libreria' element={<Libreria />} />
-      <Route path='/dashboard' element={<Dashboard/>} />
-      <Route path='/usuarios' element={<TablaUsers/>} />
-      <Route path='*' element={<Error/>} />
-    </Routes>
->>>>>>> 03cdda33d1082e87944dc4d99f616b2696aae244
     </BrowserRouter>
   );
 }
